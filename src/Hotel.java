12bdf2955ac28744ab125ae7661c7d477a1ccf17
ricardodev10 <ricardodev10@yahoo.com>
2024-08-11 12:04:21 -0300

    add class Quarto

---
+++ src/Hotel.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hotel {
+    private String nome;
+    private String endereco;
+    private List<Quarto> quartos;
+
+    public Hotel(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.quartos = new ArrayList<>();
+    }
+
+    public List<Quarto> listarQuartos() {
+        return quartos;
+    }
+
+    public void adicionarQuarto(Quarto quarto) {
+        quartos = new ArrayList();
+        quartos.add(quarto);
+    }
+
+    public void removerQuarto(Quarto quarto) {
+        quartos.remove(quarto);
+    }
+
+    public Quarto buscarQuarto(int numero) {
+        for (int i = 1; i < this.quartos.size(); i++) {
+            if (this.quartos.get(i).getNumero() == numero) {
+                return this.quartos.get(numero);
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/Quarto.java
@@ -0,0 +1,31 @@
+public class Quarto {
+    private int numero;
+    private double precoDiaria;
+    private boolean disponivel;
+
+    public Quarto(int numero, double precoDiaria) {
+        this.numero = numero;
+        this.precoDiaria = precoDiaria;
+        this.disponivel = true;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public double getPrecoDiaria() {
+        return this.precoDiaria;
+    }
+
+    public void reservar() {
+        disponivel = false;
+    }
+
+    public void liberar() {
+        if (!disponivel) {
+            disponivel = true;
+        } else {
+            System.out.println("Quarto já está disponível.");
+        }
+    }
+}
